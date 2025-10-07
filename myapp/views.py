d712e4341df0dedde5f809617c6d4bb39142c006
Th1sMaxOn <93715332+Th1sMaxOn@users.noreply.github.com>
2025-10-07 12:23:41 +0300

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
-from flask import jsonify, request
-from . import app
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
 
 @app.get("/healthcheck")
 def healthcheck():
@@ -10,13 +11,17 @@ def healthcheck():
         "time": datetime.now(timezone.utc).isoformat()
     }), 200
 
+
 @app.get("/hello")
 def hello():
     name = request.args.get("name", "World")
     return jsonify({"message": f"Hello, {name}!"}), 200
 
-app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({"info": "Welcome to Flask Lab 1! Use /hello or /healthcheck"})
+    return jsonify({"info": "Welcome to Flask Lab 1! Use /hello or /healthcheck"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
